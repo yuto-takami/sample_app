d001614b821f07444963366ff3a00417bf087358
EC2 Default User <ec2-user@ip-172-31-53-43.ec2.internal>
2021-06-09 04:34:26 +0000

    [Finish]2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -15,17 +15,25 @@ class TodolistsController < ApplicationController
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
-    @list = List.find(params[:])
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
 
   private
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   #.../todolists/1や.../todolists/3に該当する
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
